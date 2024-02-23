611cc8d3998a139e50ed4d9d5b49b856fa8919b3
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-23 09:55:02 -0600

    Add files via upload

---
+++ RGB.ino
@@ -0,0 +1,16 @@
+int R = 5;
+int G = 6;
+int B = 9;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(R,OUTPUT);
+pinMode(G,OUTPUT);
+pinMode(B,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int rBrightness = 0;rBrightness < 255;rBrightness++)
+    analogWrite(G,rBrightness);
+    delay(10);
+}
